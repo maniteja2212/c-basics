b69e4fc2c334d0a4f343fd8300edc33d59ec07a3
maniteja2212 <92910472+maniteja2212@users.noreply.github.com>
2021-10-29 23:25:13 -0700

    Add files via upload

---
+++ mani teja 2.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+	//variabels
+	int num1, num2, sum;
+	
+	//intialization
+	num1=2250;
+	num2= 2055;
+	
+	//calculation
+	sum =num1+num2;
+	
+	printf("sum of %d and  %d is %d",num1, num2, sum);
+}
