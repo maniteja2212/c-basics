068ccdda29579dd9a9626a0e1f4ab352f12ba624
maniteja2212 <92910472+maniteja2212@users.noreply.github.com>
2021-11-11 17:22:33 +0530

    Add files via upload

---
+++ Untitled2.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+	int s,a,p;
+	printf("enter side");
+	scanf("%d", &s);
+	a=s*s;
+	p=4*s;
+	printf("area of square %d\n",a);
+	printf("perimeter of square %d",p);
+}
